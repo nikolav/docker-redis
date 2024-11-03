b5f275d15dc37bbfc462734f454fb5142b57a05e
Vukovic Nikola <admin@nikolav.rs>
2024-11-03 11:18:04 +0100

    Sun Nov  3 10:18:04 UTC 2024

--- cat-config.sh
+++ cat-config.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-
-echo '@@ ./wserver.sh'
-cat ./wserver.sh
-
 echo '@@ ./.env'
 cat ./.env
 
 echo '@@ ./redis/redis.conf'
 cat ./redis/redis.conf
 
+echo '@@ ./wserver.sh'
+cat ./wserver.sh
 
